c113786235f691d8b5d5dc84861658a811168527
JesperKYH <jesper.pettersson@student.kyh.se>
2020-10-02 12:10:39 +0200

    Added so text gets added to msg-output

--- main.js
+++ main.js
@@ -8,23 +8,26 @@ const msg = document.querySelector("#msg");
 const form = document.querySelector("form");
 let msgOutput = document.querySelector(".msg-output");
 
-let msgValue = msg.value();
+
 
 function addMsg(msgValue){
     let html = `
-        <div>
+        <div class = "msg-text">
             <p>${msgValue}</p>
         </div>
     `;
 
     //*Add the html to the msgOutput.
-    msgOutput.innerHTML += html;
+    //* Adds it to the start/begining of the parent div (div in this case, can be child of anything)
+    msgOutput.insertAdjacentHTML("afterbegin", html)
     
 }
 
 form.addEventListener("submit", (e) => {
-
     e.preventDefault();
 
-    addMsg();
+    let msgValue = form.msg.value;
+
+    addMsg(msgValue);
+    form.reset();
 })
