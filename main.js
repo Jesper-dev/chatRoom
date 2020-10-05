8c3c0dbe18d0a9a93aff9650707cf5c4f558b6b1
JesperKYH <jesper.pettersson@student.kyh.se>
2020-10-05 13:09:05 +0200

    Started working on validation on msg

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function clg(object){
+function clg(object) {
     console.log(object);
 }
 
@@ -7,9 +7,12 @@ const msg = document.querySelector("#msg");
 // const submit = document.querySelector("#submit");
 const form = document.querySelector("form");
 let msgOutput = document.querySelector(".msg-output");
+let msgInput = document.querySelector("#msg");
+
+
 
 //* Adda message to msgOutput div
-function addMsg(msgValue, timeDiffer, differ, now){
+function addMsg(msgValue, timeDiffer, differ, now) {
 
     let html = `
         <div class = "msg-text">
@@ -21,17 +24,29 @@ function addMsg(msgValue, timeDiffer, differ, now){
     //*Add the html to the msgOutput.
     //* Adds it to the start/begining of the parent div (div in this case, can be child of anything)
     msgOutput.insertAdjacentHTML("afterbegin", html)
-    
+
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    
-    let now = new Date();
-    let differ = new Date();
-    let timeDiffer = dateFns.distanceInWords(differ, now, {addSuffix: true});
-    let msgValue = form.msg.value;
 
-    addMsg(msgValue, timeDiffer, differ, now);
+    if (msgInput.value == "") {
+        msgInput.classList.add("error");
+    } else {
+        msgInput.classList.remove("error");
+        let now = new Date();
+        let differ = new Date();
+        let timeDiffer = dateFns.distanceInWords(differ, now, { addSuffix: true });
+        let msgValue = form.msg.value;
+
+        addMsg(msgValue, timeDiffer, differ, now);
+    }
+
     form.reset();
+});
+
+form.addEventListener("keyup", () => {
+    if (msgInput.value == "") {
+        msgInput.classList.add("error");
+    }
 });
