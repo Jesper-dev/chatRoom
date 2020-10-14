9796f8771af67c05452a210e81b9cc3f04a027da
JesperKYH <jesper.pettersson@student.kyh.se>
2020-10-14 16:13:05 +0200

    Errors on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     entry_chat: "./src/chat.js",
@@ -10,5 +11,20 @@ module.exports = {
     output_users: {
         path: path.resolve(__dirname, "dist/assets"),
         filename: "users_after.js"
+    },
+    devServer: {
+        inline: true,
+        contentBase: __dirname + "/dist",
+        port: 3000
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: path.resolve(__dirname, "./node_modules/babel-loader"),
+            query: {
+                presets: ["latest", "stage-0", "react"]
+            }
+        }]
     }
 };
